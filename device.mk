cd7e0d65c6361a8d7677109bef30bf243e9d3da6
Samuel Kendall <kinguser981@gmail.com>
2024-10-14 19:22:45 +0330

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,7 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # API level
